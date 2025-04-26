75c07994c1bccda10e8c549e67e2c9bb914503a1
arturocev <arturo_garcia_m-tsapp1oa2324@cev.com>
2025-04-26 14:22:26 +0200

    quitadas las comillas sobrantes en terminos y politica de privacidad

--- lib/screens/ajustes.dart
+++ lib/screens/ajustes.dart
@@ -155,29 +155,29 @@ class _AjustesScreenState extends State<AjustesScreen> {
         content: SingleChildScrollView(
           child: Text(
             '''Última actualización: [06/03/2025]
- Bienvenido a Mandangon. Antes de utilizar nuestros servicios, te pedimos que leas y aceptes los siguientes términos y condiciones\n"
-            "1. Definiciones\n"
-            "- Plataforma: Aplicación móvil y web de Mandangon que permite a los usuarios acceder a recetas y localizar restaurantes cercanos.\n"
-            "- Usuario: Persona que utiliza la plataforma para visualizar recetas o encontrar restaurantes.\n"
-            "- Restaurante asociado: Establecimiento que proporciona información sobre su ubicación, contacto y servicios.\n"
-            "2. Uso de la Plataforma\n"
-            "- Se prohíbe el uso de la plataforma para actividades fraudulentas o ilegales.\n"
-            "- Mandangon no es responsable de la disponibilidad o calidad de los productos de los restaurantes ni de los ingredientes de las recetas.\n"
-            "3. Recetas\n"
-            "- Cada receta incluye una imagen, una lista de ingredientes y pasos detallados de preparación.\n"
-            "- La información proporcionada es orientativa y puede variar según los ingredientes o la preparación.\n"
-            "- [Mandangon] no se responsabiliza por alergias o problemas de salud derivados del consumo de los platos preparados.\n"
-            "4. Restaurantes Cercanos\n"
-            "- La plataforma muestra restaurantes basados en la ubicación del usuario.\n"
-            "- Cada restaurante puede incluir información como dirección, teléfono, horario y servicios disponibles.\n"
-            "- Mandangon no garantiza la exactitud de la información ni la disponibilidad de los servicios en los restaurantes listados.\n"
-            "5. Responsabilidad y Limitaciones\n"
-            "- Mandangon actúa como intermediario y no es responsable por problemas en la información de recetas o restaurantes.\n"
-            "- No garantizamos la disponibilidad constante de la plataforma y nos reservamos el derecho de modificar o suspender servicios en cualquier momento.\n"
-            "6. Modificaciones de los Términos\n"
-            "- Nos reservamos el derecho de modificar estos términos cuando sea necesario.\n"
-            "- El uso continuado de la plataforma después de los cambios implica la aceptación de los nuevos términos.\n"
-            "Si tienes dudas, contáctanos en soporte@mandangon.com.'''
+Bienvenido a Mandangon. Antes de utilizar nuestros servicios, te pedimos que leas y aceptes los siguientes términos y condiciones\n
+            1. Definiciones\n
+              - Plataforma: Aplicación móvil y web de Mandangon que permite a los usuarios acceder a recetas y localizar restaurantes cercanos.\n
+              - Usuario: Persona que utiliza la plataforma para visualizar recetas o encontrar restaurantes.\n
+              - Restaurante asociado: Establecimiento que proporciona información sobre su ubicación, contacto y servicios.\n
+            2. Uso de la Plataforma\n
+              - Se prohíbe el uso de la plataforma para actividades fraudulentas o ilegales.\n
+              - Mandangon no es responsable de la disponibilidad o calidad de los productos de los restaurantes ni de los ingredientes de las recetas.\n
+            3. Recetas\n
+              - Cada receta incluye una imagen, una lista de ingredientes y pasos detallados de preparación.\n
+              - La información proporcionada es orientativa y puede variar según los ingredientes o la preparación.\n
+              - [Mandangon] no se responsabiliza por alergias o problemas de salud derivados del consumo de los platos preparados.\n
+            4. Restaurantes Cercanos\n
+              - La plataforma muestra restaurantes basados en la ubicación del usuario.\n
+              - Cada restaurante puede incluir información como dirección, teléfono, horario y servicios disponibles.\n
+              - Mandangon no garantiza la exactitud de la información ni la disponibilidad de los servicios en los restaurantes listados.\n
+            5. Responsabilidad y Limitaciones\n
+              - Mandangon actúa como intermediario y no es responsable por problemas en la información de recetas o restaurantes.\n
+              - No garantizamos la disponibilidad constante de la plataforma y nos reservamos el derecho de modificar o suspender servicios en cualquier momento.\n
+            6. Modificaciones de los Términos\n
+              - Nos reservamos el derecho de modificar estos términos cuando sea necesario.\n
+              - El uso continuado de la plataforma después de los cambios implica la aceptación de los nuevos términos.\n
+            Si tienes dudas, contáctanos en soporte@mandangon.com.'''
           ),
         ),
         actions: [
@@ -198,28 +198,28 @@ class _AjustesScreenState extends State<AjustesScreen> {
         content: SingleChildScrollView(
           child: Text(
             '''Última actualización: [06/03/2025]
-  En Mandangon, nos tomamos muy en serio la privacidad de nuestros usuarios. Esta Política de Privacidad describe cómo recopilamos, usamos y protegemos tu información personal cuando utilizas nuestra aplicación.\n"
-            "1. Información que Recopilamos\n"
-            "- Información personal: Nombre, correo electrónico, teléfono (si decides registrarte).\n"
-            "- Datos de uso: Información sobre cómo interactúas con la aplicación, como recetas consultadas o restaurantes visitados.\n"
-            "2. Uso de la Información\n"
-            "- Ofrecerte una mejor experiencia, permitiéndote acceder a recetas y restaurantes cercanos de manera personalizada.\n"
-            "- Mejorar nuestra plataforma mediante análisis del comportamiento de los usuarios.\n"
-            "- Enviarte notificaciones sobre actualizaciones o promociones (si decides aceptarlas).\n"
-            "3. Compartición de Datos\n"
-            "- No vendemos ni compartimos tu información personal con terceros, salvo en los siguientes casos:\n"
-            "  - Con proveedores de servicios que nos ayudan a operar la plataforma (por ejemplo, servidores de almacenamiento).\n"
-            "  - Si es requerido por ley o por una solicitud legal válida.\n"
-            "4. Seguridad de la Información\n"
-            "- Implementamos medidas de seguridad para proteger tu información, pero no podemos garantizar una seguridad absoluta. Se recomienda no compartir datos sensibles en la plataforma.\n"
-            "5. Permisos y Control de Datos\n"
-            "- Puedes gestionar los permisos otorgados a la aplicación en los ajustes de tu dispositivo. También puedes:\n"
-            "  - Modificar o eliminar tu cuenta en cualquier momento.\n"
-            "  - Desactivar el acceso a la ubicación desde los ajustes del teléfono.\n"
-            "  - Solicitar la eliminación de tus datos contactándonos en soporte@mandangon.com.\n"
-            "6. Cambios en la Política de Privacidad\n"
-            "- Nos reservamos el derecho de modificar esta política en cualquier momento. Te notificaremos sobre cambios importantes a través de la aplicación.\n"
-            "Si tienes dudas, contáctanos en soporte@mandangon.com.'''
+En Mandangon, nos tomamos muy en serio la privacidad de nuestros usuarios. Esta Política de Privacidad describe cómo recopilamos, usamos y protegemos tu información personal cuando utilizas nuestra aplicación.\n
+            1. Información que Recopilamos\n
+              - Información personal: Nombre, correo electrónico, teléfono (si decides registrarte).\n
+              - Datos de uso: Información sobre cómo interactúas con la aplicación, como recetas consultadas o restaurantes visitados.\n
+            2. Uso de la Información\n
+              - Ofrecerte una mejor experiencia, permitiéndote acceder a recetas y restaurantes cercanos de manera personalizada.\n
+              - Mejorar nuestra plataforma mediante análisis del comportamiento de los usuarios.\n
+              - Enviarte notificaciones sobre actualizaciones o promociones (si decides aceptarlas).\n
+            3. Compartición de Datos\n
+              - No vendemos ni compartimos tu información personal con terceros, salvo en los siguientes casos:\n
+              - Con proveedores de servicios que nos ayudan a operar la plataforma (por ejemplo, servidores de almacenamiento).\n
+              - Si es requerido por ley o por una solicitud legal válida.\n
+            4. Seguridad de la Información\n
+              - Implementamos medidas de seguridad para proteger tu información, pero no podemos garantizar una seguridad absoluta. Se recomienda no compartir datos sensibles en la plataforma.\n
+            5. Permisos y Control de Datos\n
+              - Puedes gestionar los permisos otorgados a la aplicación en los ajustes de tu dispositivo. También puedes:\n
+              - Modificar o eliminar tu cuenta en cualquier momento.\n
+              - Desactivar el acceso a la ubicación desde los ajustes del teléfono.\n
+              - Solicitar la eliminación de tus datos contactándonos en soporte@mandangon.com.\n
+            6. Cambios en la Política de Privacidad\n
+              - Nos reservamos el derecho de modificar esta política en cualquier momento. Te notificaremos sobre cambios importantes a través de la aplicación.\n
+            Si tienes dudas, contáctanos en soporte@mandangon.com.'''
           ),
         ),
         actions: [
